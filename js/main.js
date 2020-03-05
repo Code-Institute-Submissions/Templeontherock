671643c987ba5726972bf2ef3400fadd508e1a0f
anthony keogh <siliconirish@gmal.com>
2020-03-05 11:26:43 +0000

    hello

---
+++ js/main.js
@@ -0,0 +1,37 @@
+function mOver(obj) {
+    obj.innerHTML = "Luke Ryan"
+   
+    document.getElementById("event-dom-name").setAttribute(
+  "style","font-size: 50px; font-style: italic; color:blue; text-shadow: 2px 2px black;"
+      );
+  }
+
+  ///comments
+  function mOut(obj) {
+    obj.innerHTML = "Name"
+  } 
+  
+  function mOver2(obj) {
+    obj.innerHTML = "Lisa Thompson"
+    document.getElementById("event-dom-name-2").setAttribute(
+  "style","font-size: 50px; font-style: italic; color:blue; text-shadow: 2px 2px black;"
+      );
+    
+  
+  }
+  
+  function mOver3(obj) {
+    obj.innerHTML = "George O'mara"
+   document.getElementById("event-dom-name-3").setAttribute(
+  "style","font-size: 50px; font-style: italic; color:blue; text-shadow: 2px 2px black;"
+      );
+  
+  }
+
+  function mOver4(obj) {
+    obj.innerHTML = "NEW ALBUM"
+   document.getElementById("event-album").setAttribute(
+  "style","font-size: 50px; margin-left:100px; font-style: italic; color:blue;"
+      );
+  
+  }
